7cf5e4cb8e216b11f7bbee9ea2b9a1c98b25c68f
weimuhua <weimuhua0812@126.com>
2017-05-10 23:30:40 +0800

    change : change bash_profile.sh.

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,18 +1,19 @@
-export PATH=${PATH}:/Users/baidu/Android/adt-bundle-mac-x86_64-20140702/sdk/platform-tools
-export PATH=${PATH}:/Users/baidu/Android/adt-bundle-mac-x86_64-20140702/sdk/tools
-export ANDROID_HOME=/Users/baidu/Android/adt-bundle-mac-x86_64-20140702/sdk
+export ANDROID_HOME=/Users/Wayne/Android/sdk/
+export PATH=${PATH}:/Users/Wayne/Android/sdk/platform-tools
+export PATH=${PATH}:/Users/Wayne/Android/sdk/tools
+export PATH=${PATH}:/Users/Wayne/Android/sdk/build-tools
+
 export ANT_HOME=/usr/local/apache-ant-1.9.4
-export NDK_HOME=/Users/baidu/Android/android-ndk-r10d
 export PATH=${PATH}:${ANT_HOME}/bin
-export PATH=${PATH}:/Users/baidu/Android/android-ndk-r10d
+
+export NDK_HOME=/Users/baidu/Android/android-ndk-r10d
+export PATH=${PATH}:${NDK_HOME}
+
 export PS1='[\u @ \w] $ '
+
 source ~/.git-completion.bash
+
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
-export ANDROID_NDK=/Users/baidu/Android/android-ndk-r10d
-export CC=${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.6/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gcc
-export CCOPT="-O2 -fpic --sysroot=/home/ducalpha/adr/ndk/platforms/android-14/arch-arm -DANDROID -DOS_ANDROID"
-export CFLAGS="--sysroot=${ANDROID_NDK}/platforms/android-14/arch-arm -DANDROID -DOS_ANDROID"
-export LDFLAGS="--sysroot=${ANDROID_NDK}/platforms/android-14/arch-arm -fPIC -mandroid -L${ANDROID_NDK}/platforms/android-14/arch-arm/usr/lib"
+alias grep='grep --color'
