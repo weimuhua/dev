cdd936d5da7e30bd83e2628953148a926680b498
weimuhua <weimuhua0812@126.com>
2017-07-01 17:52:34 +0800

    Update bash_profile.sh

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,17 +1,30 @@
-export ANDROID_HOME=/Users/Wayne/Android/sdk/
+export JAVA_7_HOME=`/usr/libexec/java_home -v 1.7`
+export JAVA_8_HOME=`/usr/libexec/java_home -v 1.8`
+
+alias jdk7="export JAVA_HOME=$JAVA_7_HOME"
+alias jdk8="export JAVA_HOME=$JAVA_8_HOME"
+#export JAVA_HOME=$JAVA_7_HOME
+export JAVA_HOME=$JAVA_8_HOME
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+
+
+export ANDROID_HOME=/Users/wayne/Android/sdk
 export PATH=${PATH}:/Users/Wayne/Android/sdk/platform-tools
 export PATH=${PATH}:/Users/Wayne/Android/sdk/tools
 export PATH=${PATH}:/Users/Wayne/Android/sdk/build-tools
+export android_home=$ANDROID_HOME
+export proguard_home=$android_home/tools/proguard
 
-export ANT_HOME=/usr/local/apache-ant-1.9.4
+
+export ANT_HOME=/Users/wayne/apache-ant-1.9.9
 export PATH=${PATH}:${ANT_HOME}/bin
+ANT_OPTS=-Xmx2g
 
-export NDK_HOME=/Users/baidu/Android/android-ndk-r10d
-export PATH=${PATH}:${NDK_HOME}
 
-export PS1='[\u @ \w] $ '
+#export PS1='[\u @ \w] $ '
+#source ~/.git-completion.bash
 
-source ~/.git-completion.bash
 
 alias ll='ls -alF'
 alias la='ls -A'
